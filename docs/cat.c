2c85f3c316d0862a49f32293cc54d3f9d66691e8
archer <leetpwnsroot@gmail.com>
2022-07-12 13:41:35 +0200

    Optimized cat using fstat syscall

--- docs/cat.c
+++ docs/cat.c
@@ -7,33 +7,40 @@
 #include <fcntl.h>
 
 int main(int argc, char **argv) {
+    int fd;
+    struct stat buf;
+
     if (argc != 2) {
         printf("Error\n");
         printf("Usage: %s <a text file to read>\n", argv[0]);
         exit(127);
     }
 
-    int fd = open(argv[1], O_RDONLY);
+    fd = open(argv[1], O_RDONLY);
+
     if (fd == -1) {
         printf("could not open file\n");
-        exit(1127);
+        exit(127);
     }
 
     const size_t buffer_size = 16;
-    char *buffer = malloc(buffer_size);
-
-    while (true) {
-        ssize_t size = read(fd, buffer, buffer_size);
-
-        if (size == -1) {
-            printf("an error happened while reading the file");
-            exit(127);
-        }
-        if (size == 0) {
-            break;
-        }
-        write(STDOUT_FILENO, buffer, size);
+    if (fstat(fd, &buf) != 0)
+    {
+        printf("Failed to stat a file");
+        exit(127);
     }
+    
+    char *buffer = malloc(buf.st_size);
+
+    ssize_t size = read(fd, buffer, buf.st_size);
+
+    if (size == -1) {
+        printf("an error happened while reading the file");
+        exit(127);
+    }
+    
+    write(STDOUT_FILENO, buffer, size);
+    
     
     write(STDOUT_FILENO, "\n", 1);
     return (0);
